61c86dfd6f9c0481694127b2215ae4da3c4471f2
Mr-Ojii <okaschan@gmail.com>
2025-08-24 00:40:04 +0900

    AviUtl2: Fix the progress dialog was not displayed.

--- AviUtl2/lwlibav_input.c
+++ AviUtl2/lwlibav_input.c
@@ -225,7 +225,7 @@ static void *open_file( char *file_path, reader_option_t *opt )
     indicator.update = update_indicator;
     indicator.close  = close_indicator;
     progress_handler_t ph = { { 0 } };
-    ph.module_name = "lwinput.aui";
+    ph.module_name = "lwinput.aui2";
     ph.template_id = IDD_PROGRESS_ABORTABLE;
     /* Construct index. */
     if( lwlibav_construct_index( &hp->lwh, hp->vdhp, hp->vohp, hp->adhp, hp->aohp, vlhp, &lwlibav_opt, &indicator, &ph ) < 0 )
