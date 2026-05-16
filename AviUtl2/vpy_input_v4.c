88aa8f6f88ce95142051293bff18ed1bfc00525a
Mr-Ojii <okaschan@gmail.com>
2026-05-16 22:48:54 +0900

    AviUtl2: Support VapourSynth API v4

---
+++ AviUtl2/vpy_input_v4.c
@@ -0,0 +1,418 @@
+/********************************************************************************
+ * vpy_input_v4.c
+ ********************************************************************************
+ * Copyright (C) 2013-2015 L-SMASH Works project
+ *
+ * Authors: Yusuke Nakamura <muken.the.vfrmaniac@gmail.com>
+ *          Mr-Ojii <contact.mr.ojii@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *****************************************************************************/
+
+/* This file is available under an ISC license.
+ * However, when distributing its binary file, it will be under LGPL or GPL.
+ * Don't distribute it if its license is GPL. */
+
+#include "vpy_input.h"
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#include <VSScript4.h>
+#pragma GCC diagnostic pop
+
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+#include <winuser.h>
+
+#include "video_output.h"
+
+vpy_reader_t vpy_input_v4_wrapper;
+
+typedef enum VSEvalFlags
+{
+    efSetWorkingDir = 1,
+} VSEvalFlags;
+
+#define VSSCRIPT_API( ret, name ) typedef ret (VS_CC *name##_func)
+VSSCRIPT_API( const VSSCRIPTAPI *, getVSScriptAPI )( int version );
+#undef VSSCRIPT_API
+
+typedef struct
+{
+    /* VSScript */
+    HMODULE                   library;
+    struct
+    {
+        VSScript *handle;
+        struct
+        {
+#define VSSCRIPT_DECLARE_FUNC( name ) name##_func name
+            VSSCRIPT_DECLARE_FUNC( getVSScriptAPI );
+#undef VSSCRIPT_DECLARE_FUNC
+        } func;
+    } vsscript;
+    /* VapourSynth */
+    const VSSCRIPTAPI        *vssapi;
+    const VSAPI              *vsapi;
+    VSScript                 *script;
+    VSNode                   *node;
+    const VSVideoInfo        *vi;
+    /* Video stuff */
+    AVFrame                  *av_frame;
+    lw_video_output_handler_t voh;
+} vpy_v4_handler_t;
+
+static int load_vsscript_dll
+(
+    vpy_v4_handler_t *v4hp
+)
+{
+    HKEY  key_handle;
+    DWORD data_size;
+    
+    if( RegOpenKeyExA( HKEY_LOCAL_MACHINE, "Software\\VapourSynth", 0, KEY_QUERY_VALUE, &key_handle ) == ERROR_SUCCESS
+     && RegQueryValueExA( key_handle, "VSScriptDLL", NULL, NULL, NULL, &data_size ) == ERROR_SUCCESS )
+    {
+        char dll_path[data_size];
+        if( RegQueryValueExA( key_handle, "VSScriptDLL", NULL, NULL, (LPBYTE)dll_path, &data_size ) == ERROR_SUCCESS )
+            v4hp->library = LoadLibraryA( dll_path );
+        RegCloseKey( key_handle );
+    }
+    if ( GetEnvironmentVariableW( L"VSSCRIPT_PATH", NULL, 0 ) )
+    {
+        wchar_t lpBuffer[1024];
+        GetEnvironmentVariableW( L"VSSCRIPT_PATH", lpBuffer, sizeof(lpBuffer) );
+        v4hp->library = LoadLibraryW( lpBuffer );
+    }
+    if( !v4hp->library )
+        v4hp->library = LoadLibraryA( "vsscript" );
+    if( !v4hp->library )
+        return -1;
+#define SYM( name, size ) LW_STRINGFY( name )
+#define LOAD_VSSCRIPT_FUNC( name, size )                                              \
+    do                                                                                \
+    {                                                                                 \
+        v4hp->vsscript.func.name =                                                    \
+            (name##_func)GetProcAddress( v4hp->library, SYM( name, size ) );          \
+        if( !v4hp->vsscript.func.name )                                               \
+            goto fail;                                                                \
+    } while( 0 )
+    LOAD_VSSCRIPT_FUNC( getVSScriptAPI,     4  );
+#undef SYM
+#undef LOAD_VSSCRIPT_FUNC
+    return 0;
+fail:
+    FreeLibrary( v4hp->library );
+    return -1;
+}
+
+static void close_vsscript_dll
+(
+    vpy_v4_handler_t *v4hp
+)
+{
+    assert( v4hp->library );
+    v4hp->node = NULL;
+    v4hp->script = NULL;
+    v4hp->vsscript.handle = NULL;
+    FreeLibrary( v4hp->library );
+}
+
+static enum AVPixelFormat vs_to_av_input_pixel_format
+(
+    const VSVideoFormat *format
+)
+{
+    if( !format )
+        return AV_PIX_FMT_NONE;
+    VSPresetVideoFormat vs_input_pixel_format = (VSPresetVideoFormat)(format->colorFamily << 28 | format->sampleType << 24 | format->bitsPerSample << 16 | format->subSamplingW << 8 | format->subSamplingH << 0);
+    static const struct
+    {
+        VSPresetVideoFormat vs_input_pixel_format;
+        enum AVPixelFormat av_input_pixel_format;
+    } format_table[] =
+        {
+            { pfYUV420P8,  AV_PIX_FMT_YUV420P     },
+            { pfYUV422P8,  AV_PIX_FMT_YUV422P     },
+            { pfYUV444P8,  AV_PIX_FMT_YUV444P     },
+            { pfYUV410P8,  AV_PIX_FMT_YUV410P     },
+            { pfYUV411P8,  AV_PIX_FMT_YUV411P     },
+            { pfYUV440P8,  AV_PIX_FMT_YUV440P     },
+            { pfYUV420P9,  AV_PIX_FMT_YUV420P9LE  },
+            { pfYUV422P9,  AV_PIX_FMT_YUV422P9LE  },
+            { pfYUV444P9,  AV_PIX_FMT_YUV444P9LE  },
+            { pfYUV420P10, AV_PIX_FMT_YUV420P10LE },
+            { pfYUV422P10, AV_PIX_FMT_YUV422P10LE },
+            { pfYUV444P10, AV_PIX_FMT_YUV444P10LE },
+            { pfYUV420P16, AV_PIX_FMT_YUV420P16LE },
+            { pfYUV422P16, AV_PIX_FMT_YUV422P16LE },
+            { pfYUV444P16, AV_PIX_FMT_YUV444P16LE },
+            { pfRGB24,     AV_PIX_FMT_GBRP        },
+            { pfRGB27,     AV_PIX_FMT_GBRP9LE     },
+            { pfRGB30,     AV_PIX_FMT_GBRP10LE    },
+            { pfRGB48,     AV_PIX_FMT_GBRP16LE    },
+            { pfGray8,     AV_PIX_FMT_GRAY8       },
+            { pfGray16,    AV_PIX_FMT_GRAY16LE    },
+            { pfNone,      AV_PIX_FMT_NONE        }
+        };
+    for( int i = 0; format_table[i].vs_input_pixel_format != pfNone; i++ )
+        if( vs_input_pixel_format == format_table[i].vs_input_pixel_format )
+            return format_table[i].av_input_pixel_format;
+    return AV_PIX_FMT_NONE;
+}
+
+static int prepare_video_decoding
+(
+    lsmash_handler_t *h,
+    video_option_t   *opt
+)
+{
+    vpy_handler_t *hp = (vpy_handler_t *)h->video_private;
+    vpy_v4_handler_t *v4hp = (vpy_v4_handler_t *)hp->private_stuff;
+    h->video_sample_count = v4hp->vi->numFrames;
+    h->framerate_num      = v4hp->vi->fpsNum;
+    h->framerate_den      = v4hp->vi->fpsDen;
+    /* Set up video rendering. */
+    enum AVPixelFormat input_pixel_format = vs_to_av_input_pixel_format( &v4hp->vi->format );
+    return au_setup_video_rendering( &v4hp->voh, opt, &h->video_format, v4hp->vi->width, v4hp->vi->height, input_pixel_format );
+}
+
+static inline int vs_is_rgb_format
+(
+    int color_family
+)
+{
+    return color_family == cfRGB ? 1 : 0;
+}
+
+static inline void get_color_range
+(
+    vpy_v4_handler_t *v4hp,
+    const VSMap   *props
+)
+{
+    if( v4hp->vsapi->mapNumElements( props, "_ColorRange" ) > 0 )
+        v4hp->av_frame->color_range = v4hp->vsapi->mapGetInt( props, "_ColorRange", 0, NULL )
+                             ? AVCOL_RANGE_MPEG
+                             : AVCOL_RANGE_JPEG;
+    else
+        v4hp->av_frame->color_range = AVCOL_RANGE_UNSPECIFIED;
+}
+
+static inline void get_color_matrix_coefficients
+(
+    vpy_v4_handler_t *v4hp,
+    const VSMap   *props
+)
+{
+    if( v4hp->vsapi->mapNumElements( props, "_Matrix" ) > 0 )
+        v4hp->av_frame->colorspace = v4hp->vsapi->mapGetInt( props, "_Matrix", 0, NULL );
+    else
+        v4hp->av_frame->colorspace = AVCOL_SPC_UNSPECIFIED;
+}
+
+static inline void get_interlaced_info
+(
+    vpy_v4_handler_t *v4hp,
+    const VSMap   *props
+)
+{
+    if( v4hp->vsapi->mapNumElements( props, "_FieldBased" ) > 0 )
+    {
+        int64_t value = v4hp->vsapi->mapGetInt( props, "_FieldBased", 0, NULL );
+        if( value == 2 || value == 1 )
+            v4hp->av_frame->flags |= AV_FRAME_FLAG_INTERLACED;
+        else
+            v4hp->av_frame->flags &= ~AV_FRAME_FLAG_INTERLACED;
+        if( v4hp->av_frame->flags & AV_FRAME_FLAG_INTERLACED )
+        {
+            if( value == 2 )
+                v4hp->av_frame->flags |= AV_FRAME_FLAG_TOP_FIELD_FIRST;
+            else
+                v4hp->av_frame->flags &= ~AV_FRAME_FLAG_TOP_FIELD_FIRST;
+        }
+    }
+    else
+    {
+        v4hp->av_frame->flags &= ~AV_FRAME_FLAG_INTERLACED;
+        v4hp->av_frame->flags &= ~AV_FRAME_FLAG_TOP_FIELD_FIRST;
+    }
+}
+
+static vpy_handler_t *open_file
+(
+    char            *file_name,
+    reader_option_t *opt
+)
+{
+    /* Check file extension. */
+    if( lw_check_file_extension( file_name, "vpy" ) < 0 )
+        return NULL;
+    /* Try to open the file as VapourSynth script. */
+    vpy_handler_t *hp = lw_malloc_zero( sizeof(vpy_handler_t) );
+    if( !hp )
+        return NULL;
+    vpy_v4_handler_t *v4hp = lw_malloc_zero( sizeof(vpy_v4_handler_t) );
+    if( !v4hp )
+    {
+        lw_free( hp );
+        return NULL;
+    }
+    hp->reader = &vpy_input_v4_wrapper;
+    hp->private_stuff = v4hp;
+    if( load_vsscript_dll( v4hp ) < 0 )
+        goto fail;
+    v4hp->vssapi = v4hp->vsscript.func.getVSScriptAPI( VAPOURSYNTH_API_VERSION );
+    if( !v4hp->vssapi )
+        goto fail;
+    v4hp->vsapi = v4hp->vssapi->getVSAPI( VAPOURSYNTH_API_VERSION );
+    if( !v4hp->vsapi )
+        goto fail;
+    VSCore *core = v4hp->vsapi->createCore(0);
+    if( !core )
+        goto fail;
+    v4hp->script = v4hp->vssapi->createScript( core );
+    if( !v4hp->script )
+        goto fail;
+    v4hp->vssapi->evalSetWorkingDir( v4hp->script, efSetWorkingDir );
+    if ( v4hp->vssapi->evaluateFile( v4hp->script, file_name ) < 0 )
+        goto fail;
+    v4hp->node = v4hp->vssapi->getOutputNode( v4hp->script, 0 );
+    if( !v4hp->node )
+        goto fail;
+    v4hp->vi = v4hp->vsapi->getVideoInfo( v4hp->node );
+    return hp;
+fail:
+    if( v4hp && v4hp->vssapi && v4hp->script )
+        v4hp->vssapi->freeScript( v4hp->script );
+    if( v4hp->library )
+        close_vsscript_dll( v4hp );
+    lw_free( v4hp );
+    lw_free( hp );
+    return NULL;
+}
+
+static int find_video
+(
+    lsmash_handler_t *h,
+    video_option_t   *opt
+)
+{
+    vpy_handler_t *hp = (vpy_handler_t *)h->video_private;
+    vpy_v4_handler_t *v4hp = (vpy_v4_handler_t *)hp->private_stuff;
+    if( v4hp->vi->numFrames <= 0 )
+        return -1;
+
+    h->video_track_count = 1;
+    return 0;
+}
+
+static int get_video_track
+(
+    lsmash_handler_t *h,
+    reader_option_t   *opt,
+    int              index
+)
+{
+    vpy_handler_t *hp = (vpy_handler_t *)h->video_private;
+    vpy_v4_handler_t *v4hp = (vpy_v4_handler_t *)hp->private_stuff;
+    if( v4hp->vi->numFrames <= 0 )
+        return -1;
+    v4hp->av_frame = av_frame_alloc();
+    if( !v4hp->av_frame )
+        return -1;
+    return prepare_video_decoding( h, &opt->video_opt );
+}
+
+static int read_video
+(
+    lsmash_handler_t *h,
+    int               sample_number,
+    void             *buf
+)
+{
+    vpy_handler_t *hp = (vpy_handler_t *)h->video_private;
+    vpy_v4_handler_t *v4hp = (vpy_v4_handler_t *)hp->private_stuff;
+    const VSFrame *vs_frame = v4hp->vsapi->getFrame( sample_number, v4hp->node, NULL, 0 );
+    if( !vs_frame )
+        return 0;
+    const VSVideoFormat *format = v4hp->vsapi->getVideoFrameFormat( vs_frame );
+    if( !format )
+    {
+        v4hp->vsapi->freeFrame( vs_frame );
+        return 0;
+    }
+    v4hp->av_frame->format = vs_to_av_input_pixel_format( format );
+    v4hp->av_frame->width  = v4hp->vsapi->getFrameWidth ( vs_frame, 0 );
+    v4hp->av_frame->height = v4hp->vsapi->getFrameHeight( vs_frame, 0 );
+    int is_rgb = vs_is_rgb_format( format->colorFamily );
+    for( int i = 0; i < format->numPlanes; i++ )
+    {
+        static const int component_reorder[2][3] =
+            {
+                { 0, 1, 2 },    /* YUV -> YUV */
+                { 2, 0, 1 }     /* RGB -> GBR */
+            };
+        int j = component_reorder[is_rgb][i];
+        v4hp->av_frame->data    [j] = (uint8_t *)v4hp->vsapi->getReadPtr( vs_frame, i );
+        v4hp->av_frame->linesize[j] =            v4hp->vsapi->getStride ( vs_frame, i );
+    }
+    const VSMap *props = v4hp->vsapi->getFramePropertiesRO( vs_frame );
+    get_color_range              ( v4hp, props );
+    get_color_matrix_coefficients( v4hp, props );
+    get_interlaced_info          ( v4hp, props );
+    /* Here, update_scaler_configuration_if_needed() is required to activate the scaler. */
+    if( update_scaler_configuration_if_needed( &v4hp->voh.scaler, NULL, v4hp->av_frame ) < 0 )
+        return 0;
+    int frame_size = convert_colorspace( &v4hp->voh, v4hp->av_frame, buf );
+    v4hp->vsapi->freeFrame( vs_frame );
+    return frame_size;
+}
+
+static void video_cleanup
+(
+    lsmash_handler_t *h
+)
+{
+    vpy_handler_t *hp = (vpy_handler_t *)h->video_private;
+    if( !hp )
+        return;
+    vpy_v4_handler_t *v4hp = (vpy_v4_handler_t *)hp->private_stuff;
+    if( !v4hp )
+        return;
+    av_frame_free( &v4hp->av_frame );
+    lw_cleanup_video_output_handler( &v4hp->voh );
+}
+
+static void close_file
+(
+    vpy_handler_t *hp
+)
+{
+    if( !hp )
+        return;
+    vpy_v4_handler_t *v4hp = (vpy_v4_handler_t *)hp->private_stuff;
+    if( v4hp && v4hp->library )
+        close_vsscript_dll( v4hp );
+    lw_free( hp );
+}
+
+vpy_reader_t vpy_input_v4_wrapper =
+{
+    open_file,
+    find_video,
+    get_video_track,
+    read_video,
+    video_cleanup,
+    close_file,
+};
